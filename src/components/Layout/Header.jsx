3dc8cab5c90ad395a3b53fdbe3dd21bd44724d46
cpineda <cpineda@transnegrd.com>
2023-02-19 01:42:21 -0400

    Adding Modal and Cart Button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -4,12 +4,12 @@ import mealsImage from "../../assets/meals.jpg";
 import { Fragment } from "react";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicius food!"></img>
