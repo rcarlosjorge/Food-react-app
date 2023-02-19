6282ea1f374c715a53ada06641bc9f52a75a432d
cpineda <cpineda@transnegrd.com>
2023-02-19 02:34:11 -0400

    Adding reducer and item adding once

--- src/components/Meals/MealItem/MealsItem.jsx
+++ src/components/Meals/MealItem/MealsItem.jsx
@@ -1,9 +1,22 @@
 import classes from "./MealsItem.module.css";
 import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
+import CartContext from "../../../store/Cart-context";
 
 const MealsItem = (props) => {
 
-    const price = `$${props.price.toFixed(2)}`
+  const cartContext = useContext(CartContext); 
+
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -13,7 +26,7 @@ const MealsItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
